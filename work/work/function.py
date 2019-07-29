511a950524e749cfd0fdc6fb01d1a8bda1eef992
Merlinliu <Merlinliu98@foxmaill.com>
2019-07-29 21:33:26 +0800

    Initial commit

---
+++ work/work/function.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def count(request):
+    user_text = request.GET['text']
+    total_count = len(request.GET['text'])
+
+    word_dict = {}
+
+    for word in user_text:
+        if word not in word_dict:
+            word_dict[word] = 1
+        else:
+            word_dict[word] += 1
+        sorted_dict = sorted(word_dict.items(),key=lambda w:w[1],reverse=True)
+    return render(request, 'count.html',
+                  {'count': total_count, 'text': user_text,
+                   'worddict': word_dict, 'sorted': sorted_dict})
