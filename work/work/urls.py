40dda4d1baf4976285df17633d2e2bf7e4b2591d
Merlinliu <Merlinliu98@foxmaill.com>
2019-07-30 10:03:24 +0800

    主页补充

--- work/work/urls.py
+++ work/work/urls.py
@@ -20,6 +20,7 @@ from .import function
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', function.home),
+    path('count/', function.count),
     path('about/', function.about),
 
 
